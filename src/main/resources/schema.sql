174aea95fa300ba5544a46a03a78d8a5f3da8065
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-15 12:58:23 -0300

    chore: add h2 configuration

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS 'account' (
+    id bigint AUTO_INCREMENT primary key,
+    name varchar(255) not null,
+    password varchar(255) not null,
+    email varchar(255) not null
+)
