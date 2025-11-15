d06638a5d229b2048facfd39af6f482f20ca0785
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-15 13:39:55 -0300

    fix: correct sql syntax for account table in schema.sq

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS 'account' (
+CREATE TABLE IF NOT EXISTS account (
     id bigint AUTO_INCREMENT primary key,
     name varchar(255) not null,
     password varchar(255) not null,
