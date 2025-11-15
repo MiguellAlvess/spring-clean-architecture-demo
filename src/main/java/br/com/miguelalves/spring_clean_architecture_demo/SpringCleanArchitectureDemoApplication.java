c8543195706cf790a2701f93bbe335d0827d3438
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-15 12:52:34 -0300

    chore: add inital project setup

---
+++ src/main/java/br/com/miguelalves/spring_clean_architecture_demo/SpringCleanArchitectureDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.miguelalves.spring_clean_architecture_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCleanArchitectureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCleanArchitectureDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/miguelalves/spring_clean_architecture_demo/SpringCleanArchitectureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miguelalves.spring_clean_architecture_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCleanArchitectureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
