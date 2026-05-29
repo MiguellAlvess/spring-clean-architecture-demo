42688d378e680ca5305e48453b15d2940407bc5e
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-05-29 10:55:52 -0300

    test: add integration tests for person deletion

---
+++ src/test/java/br/com/miguelalves/spring_clean_architecture_demo/integration/person/DeletePersonIT.java
@@ -0,0 +1,69 @@
+package br.com.miguelalves.spring_clean_architecture_demo.integration.person;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.jupiter.api.Test;
+
+import br.com.miguelalves.spring_clean_architecture_demo.integration.config.IntegrationTestBase;
+import br.com.miguelalves.spring_clean_architecture_demo.utils.TestPersonFactory;
+import static io.restassured.RestAssured.given;
+
+class DeletePersonIT extends IntegrationTestBase {
+
+        @Test
+        void shouldDeletePersonSuccessfully() {
+                Map<String, Object> requestBody = TestPersonFactory.createPersonWithoutAddressesRequest(
+                                "Carlos Duarte",
+                                LocalDate.now().minusYears(45),
+                                "77788899900");
+                Long id = given()
+                                .body(requestBody)
+                                .when()
+                                .post(PERSON_API)
+                                .then().statusCode(201)
+                                .extract().jsonPath().getLong("id");
+                given()
+                                .pathParam("id", id)
+                                .when()
+                                .delete(PERSON_API + "/{id}")
+                                .then().log().ifValidationFails()
+                                .statusCode(204);
+                assertThat(personRepository.findById(id)).isEmpty();
+        }
+
+        @Test
+        void shouldDeletePersonCascadeAddresses() {
+                Map<String, Object> requestBody = TestPersonFactory.createPersonWithAddressesRequest(
+                                "Fernanda Pires",
+                                LocalDate.now().minusYears(38),
+                                "88899900011");
+                Long id = given()
+                                .body(requestBody)
+                                .when()
+                                .post(PERSON_API)
+                                .then().statusCode(201)
+                                .extract().jsonPath().getLong("id");
+                given()
+                                .pathParam("id", id)
+                                .when()
+                                .delete(PERSON_API + "/{id}")
+                                .then().log().ifValidationFails()
+                                .statusCode(204);
+                assertThat(personRepository.findById(id)).isEmpty();
+                assertThat(countAddresses()).isEqualTo(0);
+        }
+
+        @Test
+        void shouldReturnNotFoundWhenDeletingNonExistentPerson() {
+                given()
+                                .pathParam("id", 9999)
+                                .when()
+                                .delete(PERSON_API + "/{id}")
+                                .then().log().ifValidationFails()
+                                .statusCode(404)
+                                .body("error", equalTo("Person not found"));
+        }
+}
