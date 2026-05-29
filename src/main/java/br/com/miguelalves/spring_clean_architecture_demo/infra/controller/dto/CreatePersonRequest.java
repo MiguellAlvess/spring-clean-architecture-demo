3a3f2bf3aedc804a2952b595135ca37323294d0a
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-05-29 11:45:09 -0300

    refactor: ensure address cannot be null in CreatePersonRequest dto

--- src/main/java/br/com/miguelalves/spring_clean_architecture_demo/infra/controller/dto/CreatePersonRequest.java
+++ src/main/java/br/com/miguelalves/spring_clean_architecture_demo/infra/controller/dto/CreatePersonRequest.java
@@ -6,10 +6,11 @@ import java.util.List;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CreatePersonRequest(
-        @NotBlank String name,
-        @NotNull LocalDate birthDate,
-        @NotBlank String cpf,
-        @Valid List<AddressRequest> addresses) {
+                @NotBlank String name,
+                @NotNull LocalDate birthDate,
+                @NotBlank String cpf,
+                @NotNull(message = "addresses must not be null") @Size(min = 1, message = "addresses must have at least one address") @Valid List<AddressRequest> addresses) {
 }
